5604b3c04036b9c26f94e502680ad16655a61e43
haitham <haitham.akram@swvl.com>
2022-06-12 22:54:50 +0200

    fix test suit issue

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 8
+          Learn React
         </a>
       </header>
     </div>
